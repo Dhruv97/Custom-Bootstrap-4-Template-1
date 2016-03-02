e586e83685f619b4b7b90392811a60076f3c6f64
Dhruv Upadhyay <1997dhruv@gmail.com>
2016-03-02 14:05:03 -0500

    Added files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Dhruv on 2/10/16.
+ */
+$(document).ready(function() {
+
+
+    $('nav  a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 2000);
+                return false;
+            }
+        }
+    });
+
+
+
+    $('#cover  a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 2000);
+                return false;
+            }
+        }
+    });
+
+
+
+});
+
